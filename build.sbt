e32e32b6ee8b78c39e1bac0aee9d2db88a69d9b8
Bob R <bob.reynders@cs.kuleuven.be>
2015-02-15 13:04:40 +0100

    initial commit of macros

---
+++ build.sbt
@@ -0,0 +1,28 @@
+name := "scala-js-2-js-scala"
+
+version := "0.1-SNAPSHOT"
+
+scalaOrganization := "org.scala-lang.virtualized"
+scalaVersion := "2.10.2"
+
+isSnapshot := true
+organization := "com.github.tzbob"
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-unchecked",
+  "-feature",
+  "-Xlint"
+)
+
+libraryDependencies ++= Seq(
+  "org.scala-lang.virtualized" % "scala-compiler" % "2.10.2",
+  "org.scala-lang.virtualized" % "scala-library" % "2.10.2",
+  "org.scala-lang.virtualized" % "scala-reflect" % "2.10.2",
+  "EPFL" %% "lms" % "0.3-SNAPSHOT",
+  "EPFL" %% "js-scala" % "0.4-SNAPSHOT",
+  "org.scalamacros" %% "quasiquotes" % "2.1.0-M5",
+  "org.scala-js" %% "scalajs-stubs" % "0.6.0"
+)
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full)
---
+++ src/main/scala/Macros.scala
@@ -0,0 +1,178 @@
+import scala.reflect.macros.Context
+import scala.language.experimental.macros
+import scala.annotation.StaticAnnotation
+import scala.js.gen.js.GenBase
+
+package scalajs2jsscala {
+
+  import scala.virtualization.lms.common._
+
+  trait DelegatorLib extends Base {
+    def constant[T: Manifest](name: String): Rep[T]
+    def callDef[T: Manifest](self: Rep[Any], name: String, params: List[Rep[Any]]): Rep[T]
+    def callVal[T: Manifest](self: Rep[Any], name: String): Rep[T]
+  }
+
+  trait DelegatorExp extends DelegatorLib with BaseExp with EffectExp {
+    case class Constant[T](name: String) extends Def[T]
+    case class CallDef[T](self: Exp[Any], name: String, params: List[Exp[Any]]) extends Def[T]
+    case class CallVal[T](self: Exp[Any], name: String) extends Def[T]
+
+    def constant[T: Manifest](name: String): Exp[T] = toAtom(Constant(name))(implicitly[Manifest[T]], null)
+    def callDef[T: Manifest](self: Exp[Any], name: String, params: List[Exp[Any]]): Exp[T] =
+      reflectEffect(CallDef(self, name, params))(implicitly[Manifest[T]], null)
+    def callVal[T: Manifest](self: Exp[Any], name: String): Exp[T] =
+      reflectEffect(CallVal(self, name))(implicitly[Manifest[T]], null)
+  }
+
+  trait GenDelegator extends GenBase {
+    val IR: DelegatorExp
+    import IR._
+    override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+      case Constant(name) => emitValDef(sym, name)
+      case CallVal(self, name) =>
+        emitValDef(sym, s"${quote(self)}.$name")
+      case CallDef(self, name, params) =>
+        val paramsString = params.map(quote).mkString(", ")
+        emitValDef(sym, s"${quote(self)}.$name($paramsString)")
+      case _ => super.emitNode(sym, rhs)
+    }
+  }
+
+  private object JsProxyMacro {
+    def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+      import c.universe._
+      import Flag._
+
+      def stop(msg: String) = c.abort(c.enclosingPosition, msg)
+
+      def mkOpsName(tName: TypeName) = newTypeName(s"${tName.encoded}Ops")
+
+      def depTypeTree(tgtImpl: Template) = {
+        val Template(types, _, _) = tgtImpl
+        val dependencies = types.collect {
+          case Ident(typeName: TypeName) => typeName.toTermName
+          case AppliedTypeTree(Ident(typeName: TypeName), _) => typeName.toTermName
+        }
+        val typeList = dependencies.map { d =>
+          Select(Ident(d), newTypeName(s"${d.encoded}Lib"))
+        }
+        CompoundTypeTree(Template(typeList, emptyValDef, List()))
+      }
+
+      def createReifiedMembers(body: List[Tree]): List[ValOrDefDef] = {
+        def containsProxyAnnotation(mods: Modifiers): Boolean = mods.annotations.exists {
+          case q"new JSExport()" => true
+          case q"new scala.scalajs.js.annotation.JSExport()" => true
+          case _ => stop("Aliases in JSExport are not supported yet.")
+        }
+
+        body.collect {
+          case q"$mods val $tname: $tpt = ${ _ }" if containsProxyAnnotation(mods) =>
+            q"val $tname: Rep[$tpt] = callVal(self$$, ${tname.encoded})"
+
+          case q"$mods def $name[..$tparams](...$vparamss): $tpt = ${ _ }" if containsProxyAnnotation(mods) =>
+            val vRepParams = vparamss.map { subList =>
+              subList.map { v =>
+                q"val ${v.name}: Rep[${v.tpt}]"
+              }
+            }
+            val params = vRepParams.flatten.map(_.name)
+            q"def $name[..$tparams](...$vRepParams): Rep[$tpt] = callDef(self$$, ${name.encoded}, $params)"
+        }
+      }
+
+      def expandCompanion(target: ClassDef, mod: ModuleDef): ModuleDef = {
+        val q"$compMods object $compName extends ..$compBase { ..$compBody }" = mod
+
+        val tName = target.name
+        val tParams = target.tparams
+        val tParamsUnmodded = tParams.map {
+          case TypeDef(_, n, l, tpe) => TypeDef(Modifiers(), n, l, tpe)
+        }
+        val tNames = tParams.map(_.name)
+
+        val libName = newTypeName(s"${compName.encoded}Lib")
+
+        val opsName = mkOpsName(tName)
+        val implicitOpsName = newTermName(c.fresh)
+
+        val reifiedMembers = createReifiedMembers(target.impl.body)
+
+        val opsParents = {
+          val ClassDef(_, _, _, Template(types, _, _)) = target
+          types.collect {
+            case AppliedTypeTree(Ident(tName: TypeName), xs) =>
+              AppliedTypeTree(Ident(mkOpsName(tName)), xs)
+            case Ident(tName: TypeName) =>
+              Ident(mkOpsName(tName))
+          }
+        }
+
+        val lib = q"""trait $libName extends scalajs2jsscala.DelegatorLib { self: ${depTypeTree(target.impl)} =>
+            trait $opsName[..$tParams] extends ..$opsParents { 
+                val self$$: Rep[$tName[..$tNames]]
+                ..$reifiedMembers
+            }
+
+            import scala.language.implicitConversions
+            implicit def $implicitOpsName[..$tParamsUnmodded](x: Rep[$tName[..$tNames]]): $opsName[..$tNames] = 
+                new $opsName[..$tNames] {
+                    val self$$: Rep[$tName[..$tNames]] = x
+                }
+        }"""
+
+        q"$compMods object $compName extends ..$compBase { ..$compBody; $lib }"
+      }
+
+      def modify(target: ClassDef, mod: Option[ModuleDef]): c.Expr[Any] = {
+        val companion = mod.getOrElse(q"object ${target.name.toTermName}")
+        val modifiedCompanion = expandCompanion(target, companion)
+        c.Expr[Any](Block(List(target, modifiedCompanion), Literal(Constant(()))))
+      }
+
+      def expandSelf(mod: ModuleDef) = {
+        val q"$compMods object $compName extends ..$compBase { ..$compBody }" = mod
+
+        val staticName = newTermName(s"${compName.encoded}Rep")
+        val libName = newTypeName(s"${compName.encoded}StaticLib")
+        val opsName = newTypeName(s"${compName.encoded}StaticOps")
+        val implicitOpsName = newTermName(c.fresh)
+
+        val reifiedMembers = createReifiedMembers(mod.impl.body)
+
+        val selfType = tq"Rep[$compName.type]"
+
+        val lib = q"""trait $libName extends scalajs2jsscala.DelegatorLib {
+            val $staticName: $selfType = constant(${compName.encoded} + "()")
+            trait $opsName { 
+                val self$$: $selfType
+                ..$reifiedMembers
+            }
+
+            import scala.language.implicitConversions
+            implicit def $implicitOpsName(x: $selfType): $opsName = 
+                new $opsName {
+                    val self$$: $selfType = x
+                }
+        }"""
+
+        val modifiedCompanion = q"$compMods object $compName extends ..$compBase { ..$compBody; $lib }"
+        println(modifiedCompanion)
+        c.Expr[Any](Block(List(modifiedCompanion), Literal(Constant(()))))
+      }
+
+      annottees.map(_.tree).toList match {
+        case (companion: ModuleDef) :: Nil => expandSelf(companion)
+        case (target: ClassDef) :: Nil => modify(target, None)
+        case (target: ClassDef) :: (companion: ModuleDef) :: Nil => modify(target, Some(companion))
+      }
+    }
+  }
+
+  package annotation {
+    class JsScalaProxy(proxies: String*) extends StaticAnnotation {
+      def macroTransform(annottees: Any*) = macro JsProxyMacro.impl
+    }
+  }
+}
